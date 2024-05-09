f5fd0a6e611da708ab67b6b23da68cf18a59a251
alexdeharo99 <alexdeharo269@gmail.com>
2024-05-09 19:39:02 +0200

    Difusión Kawasaki R=1/10

--- Grid.h
+++ Grid.h
@@ -8,181 +8,74 @@
 #include <random>
 using namespace std; 
 
-class Grid{
+class Ring{
     private:
         using index_t = vector<int>::size_type; // Define index_t as the size type of vector<int>
-        vector<vector<int>> grid;
+        vector<int> ring;
         index_t size;
         mt19937_64 generator;
         uniform_real_distribution<double> r_distribution;
 
-        public:
+    public:
             // Constructor
-            Grid(index_t n, unsigned seed) : size(n), generator(seed), r_distribution(0.0, 1.0)
+            Ring(index_t n, unsigned seed) : size(n), generator(seed), r_distribution(0.0, 1.0)
         {
-            grid.resize(n, vector<int>(n));
+            ring.resize(n);
         }
 
         
-        float magnetization(int domain,float init_mg) const
-        // Magnetización del grid
+        float magnetization() const
+        // Magnetización del ring
         // Domains: completa -->3, abajo-->1, arriba-->2
         // Es const al final para poder acceder a ella dentro de otras funciones.
         {
-            int plus = floor((1 + init_mg / 2))*size;
-            int l_lim; 
-            int u_lim; 
-            if (domain==0){l_lim=0;u_lim=plus;}else if(domain==1){l_lim=plus;u_lim=size;}else if(domain==3){l_lim=0;u_lim=size;}
             double sum = 0;
-            for (int i = 0 ; i < static_cast<int>(size) ; i++)
+            for (int i = 0 ; i <static_cast<int>(size) ; i++)
             {
-                for (int j =static_cast<int>(l_lim); j <  static_cast<int>(u_lim)  ; j++)
-                {
-                    sum += grid[static_cast<index_t>(i)][static_cast<index_t>(j)];
-                }
+                sum += ring[static_cast<index_t>(i)];
             }
-            return float(sum / pow(size, 2));
+            return float(sum/static_cast<int>(size));
         }
-        
-        struct domains
-        {
-            float ur;
-            float ul;
-            float dr;
-            float dl;
-        };
-        
-        struct region{
-            float up;
-            float down;
-        };
 
-        float initialize(const string &init_options,float init_mg)
+        float initialize(const string &init_options)
         // Queremos iniciarla con magnetización inicial nula. Varios enfoques:
         // uqr, opposite sign up, pongo el signo contrario en el j+1 cuando inicialize un j;
         // ubd, until bound, magnetizo hasta 0 con un cierto error.
+        // brr, barrera, todos los ups metidos en una barrera cuadadrada centrada. El único que ahoramismo está adaptado a 1D
         {
-            if (init_options == "ubd")
+            if (init_options == "brr")
             {
+                //
+                index_t plusrange=static_cast<index_t>(floor(static_cast<int>(size/10)));  //NOT CHECKED
+
                 for (index_t i = 0; i < size; i++)
                 {
-                    grid[i][size - 1] = -1;
-                    grid[i][0] = 1;
-                    for (index_t j = 1; j < size - 1; j++)
-                    {
-                        double s;
-                        s = r_distribution(generator);
-                        if (s < 0.5)
-                        {
-                            grid[i][j] = 1;
-                        }
-                        else
-                        {
-                            grid[i][j] = -1;
-                        }
-                    }
+                    if ((i>size/2-plusrange) & (i<size/2+plusrange)){ring[i]=1;}else{ring[i]=-1;}
                 }
-                return magnetization(3,init_mg);
-                
-            }
-            else if (init_options == "uqr")
-            {
-                for (index_t i = 0; i < size; i++)
-                {
-                    grid[i][size - 1] = -1;
-                    grid[i][0] = 1;
-                    if(i%2==0)
-                    {
-                        for (index_t j = 1; j < size - 1; j++)
-                        {
-                            if (j % 2 == 0)
-                            {
-                                grid[i][j] = -1;
-                            }
-                            else
-                            {
-                                grid[i][j] = 1;
-                            }
-                        }
-                    }
-                    else{
-                        for (index_t j = 1; j < size - 1; j++)
-                        {
-                            if (j % 2 == 0)
-                            {
-                                grid[i][j] = 1;
-                            }
-                            else
-                            {
-                                grid[i][j] = -1;
-                            }
-                        }
-                    }
-                }
-                return 0.0f;
-            }
-            else if(init_options=="mgt"){
-                //Vamos a poner dominios uniformes
-                int plus_bound=floor((1+init_mg/2)*size);
-                for (index_t i=0; i<size;i++){
-                    for (index_t j=0; j<plus_bound; j++){
-                        grid[i][j]=1;
-                    }
-                    for (index_t j = plus_bound; j < size; j++){
-                        grid[i][j]=-1;
-                    }
-                }
-            }
+            }        
             return 0;
         }
 
         double energy(){ //Energy of the configuration
-            double e=0;
-            index_t i,j;
+            double e =0; //E+=-(1/2)*spin(i)*(spin(i+1)+spin(i-1))
+            index_t i;
             int left, right;  //Para poder calcular los vecinos izquierdo y derecho teniendo en cuenta CC continuas en x
 
-
-            j=0;
-            for(i=0;i<size;i++){ 
-                e+=grid[i][1]+2;
-            }
-
-            for(j=1;j<size;j++){i=0;
-                left=grid[size-1][j];
-                e+=grid[i][j]*(grid[i+1][j]+left+grid[i][j-1]+grid[i][j+1]);    //Ahoramismo se esta saliendo para j
-                for(i=1; i<size-1;i++){
-                    left=grid[i-1][j]; right=grid[i+1][j];
-                    e+=grid[i][j]*(left+right+grid[i][j-1]+grid[i][j+1]);
-                }i=size-1;                                                          //Se podría comprobar que aquí i llega hasta size-1.
-                right=grid[0][j];
-                e+=grid[i][j]*(grid[i][j-1]+grid[i][j+1]+grid[i-1][j]+right);               
-            }j=size-1;
-
             for(i=0;i<size;i++){
-                e+=2-grid[i][size-2];
+                left=ring[i-1]; right=ring[i+1];
+                if(i==0){left=ring[size];}
+                if(i==size){right=ring[0];}     
+                e+=ring[i]*(left+right);
             }
             e=-0.5*e;
-
             return e;
         }
 
-        int get(int i, int j){
-            return grid[static_cast<index_t>(i)][static_cast<index_t>(j)];
+        int get(int i){
+            return ring[static_cast<index_t>(i)];
         }
         
-        void set(int i, int j, int val){
-            grid[static_cast<index_t>(i)][static_cast<index_t>(j)]=val;
-        } 
-
-        
-
-         
-        region magnet_regions(){
-            float init_mgt;
-            region hemisferio;
-            hemisferio.up = magnetization(0, init_mgt);
-            hemisferio.down = magnetization(1, init_mgt);
-            return hemisferio;
+        void set(int i, int val){
+            ring[static_cast<index_t>(i)]=val;
         }
-
 };
--- IKawa.cpp
+++ IKawa.cpp
@@ -1,12 +1,7 @@
-// usar la libreria vector para vectores grandes
-// grid cerrado (definir función vecino o añadir un marco al grid copiando los valores de los bordes)
-// muchos pasos monte carlo L^2.
-// g++ programa.cpp -o Pprograma nohup ./Program
-//Comprobar el otro inicialization mode. HACER COPIA DEL GRID, INICIALIZAR ANTES DEL BUCLE TEMPERATURA PARA PODER COMPARAR BIEN 
-//LAS SIMULACIONES A DISTINTAS T. HACERLO REINICIALIZANDO EL GRID EN LA COPIA. VER COMO COPIAR EL GRID ENTERO.
-//Inicalización en modo QR funciona bien, probar modo continuo para el apartado 0;
-//En concreto comprobar también que magnet 0 en los fprintf(stdout) corresponden mas o menos con init_magnet
-//Esto hace que para este analisis tengamos qe redefinir los dominios de magnetización
+// PENDIENTE:
+// Comprobar la inicialización
+// La idea es hacer difusión (Kawasaki) hasta R y reacción (Glauber) en el resto
+// Condiciones de contorno periodicas
 #include <vector>
 #include<iostream>
 #include <stdio.h>
@@ -18,27 +13,24 @@
 using namespace std;
 using index_t = vector<int>::size_type; // Define index_t as the size type of vector<int>
 
-const int n = 32;            // Make n of type index_t
-const int t_max = 20;             // Tiempo
-float temperature_vals[] = {0.2f,0.3f,0.4f,0.5f,0.6f,1.0f,5.0f}; // TEMPERATURAS ENTERAS DE 1 A 10
-float max_asymetry=0.01f;
-float init_mgt=0.5;
+const int n = 1000;            // Make n of type index_t
+const int t_max = 500;        // Tiempo
+const int R = n/10;             // Alcance
 
-double p_val(int i1, int i2, int j1, int j2, float temperature);
-void print(FILE *out);
+float temperature_vals[] = {0.5f,0.8f,1.2f,1.7f,1.0f,5.0f}; // TEMPERATURAS ENTERAS DE 1 A 10
+
+double p_val(int i1, int i2, float temperature);
+void print(FILE *out,int t);
 
 unsigned seed1 = 1946402705; // semilla generador de números aleatorios
 mt19937_64 generator(seed1); // generador  mt19937_64
 
 //Tenemos varias distribuciones de numeros aleatorios
 uniform_int_distribution<int> i_distribution(0, n - 1);     //Distribución random para los i
-uniform_int_distribution<int> j_distribution(1, n - 2);     //Distribución random para los j (respetar extremos en y fijos)
 uniform_real_distribution<double> r_distribution(0., 1.);   // initialize the distribution r_distribution
 
 //Constructor
-Grid grid(n, seed1);
-
-
+Ring ring(n, seed1);
 
 int main()
 {
@@ -47,63 +39,17 @@ int main()
     float temperature;
     char filename[31]; // Restringido a 31 caracteres, entonces solo caben en el nombre temperaturas menores de 10 y con 1 cifra decimal
     
-
-    
-    
-
-    /*Apartado 2
-    Vamos a calcular la magnetización por dominios para cada temperatura:
-    El archivo de salida contiene en cada columna el promedio sobre los pasos montecarlo de la magnetización para cada dominio.
-    Distitas filas corresponden a distintas columnas. Realmente es calcular sobre las mitades superior e inferior, pero haciendo 
-    4 cuadrantes realmente tengo más información, aunque no debería ser relevante estadísticamente.
-    */
+    //Flips-data
     FILE *magnetization_data;
-    magnetization_data = fopen("./apartados/magnet_domains.dat", "w");
-
-    /*Apartado 4
-    Calculamos la energía por partícula en función de la temperatura. Esto es equivalente a calcular grid.energy()/n^2 al final de cada 
-    paso Montecarlo (aka una medida) y promediarlo sobre t_max.
-    */
-    FILE *epp;
-    epp=fopen("./apartados/energy_per_particle.dat","w");
-
-    /*Apartado 6:
-    Calor específico a partir de las fluctuaciones de energía. Teniendo en cuenta que <E> es la suma sobre todas las medidas 
-    del hamiltoniano y divididas por el nº de medidas (t_max), se ve que <E^2> es la suma de las energías al cuadrado.
-    Estos apartados, al ser simulaciones Monte Carlo, creo que en principio no pasa nada si en vez de empeze en tiempo=0
-    empiezo en tiempo=100, las diferencias deberían ser mayores. Luego ya es calcular la temperatura critica
-    */
-
-    FILE *CV_dat;
-    CV_dat=fopen("./apartados/specific_heat.dat","w");
-    
-    /*Apartado 7:
-    Calculo de la susceptibilidad a partir de las fluctuaciones de magnetización en cada dominio. ¿No falta volver a 
-    dividir por t_max en la formula? Mismo planteamiento que en el apartado 7
-    */
-
-    FILE *Chi_dat;
-    Chi_dat = fopen("./apartados/chi.dat", "w");
-
-
-    /*Apartado 8: 
-    Quiero partir de una magnetización no nula
-    */
+    magnetization_data = fopen("./resultados/magnet_domains.dat", "w");
 
     for (int t_iter = 0; t_iter < t_size; t_iter++)
     {
-        // Inicializo el grid
-        
-        for (int tries=0; tries<10;tries ++){
-            float asymetry=grid.initialize("mgt",init_mgt);
-            if (abs(asymetry)<max_asymetry){
-                break;                         
-            }
-            else{fprintf(stdout,"\n Intento %i, asimetría: %f",tries+1,asymetry);}
-        }
+        // Inicializo el ring
+        ring.initialize("brr");
 
 
-        fprintf(stdout, "\n \n \n Magnet 0: %f", grid.magnetization(3,init_mgt));
+        //fprintf(stdout, "\n \n \n Magnet 0: %f", ring.magnetization());
 
         temperature = temperature_vals[t_iter];
         sprintf(filename, "./ising_data/ising_dataT%.1f.dat", temperature); // si no funciona %i probar %d
@@ -117,102 +63,76 @@ int main()
             t_iter++;
         }
         
-        // Inicializo los resultados de la magnetización por dominios
-        Grid::region result;
-        result.up=0;result.down=0;
-        int i1,i2,j1,j2;
+        // Defino i1 e i2 para la difusión y i para la reacción 
+        //int i;
+        int i1,i2;
         double p;
         double ji;
-        double energy; double m_up; double m_down;
-        float sum_m_up=0; float sum_m_down=0; 
-        double sum_epp=0;
-        double sum_eppsrd=0;
-        double sum_msqrd_up=0; double sum_msqrd_down=0;
-        print(flips_data);
-        for (int t = 0; t < t_max; t++) // va en pasos Monte Carlo de n^2 intentos de spin flip
+        int t=0;
+        for ( t = 0; t < t_max; t++) // va en pasos Monte Carlo de n intentos de intercambio de posición o de spin flip
         {
-            for (int try_ = 0; try_ < pow(n, 2); try_++)
+            for (int try_ = 0; try_ <n; try_++)
             {
-                //Dos parejas
-                i1 = i_distribution(generator);i2 = i_distribution(generator);j1=j_distribution(generator);j2=j_distribution(generator);
-                p= p_val(i1,i2,j1,j2, temperature);
+                //Una pareja de números, uno al azar y otro un vecino al azar pero dentro del alcance.
+                i1 = i_distribution(generator);
+                uniform_int_distribution<int> vecino_distribution(i1-R, i1+R);
+                i2 = vecino_distribution(generator);
+                
+                if(i2<0){i2+=n;}else if (i2>n-1){i2-=n;}
+                
+                p= p_val(i1,i2, temperature);
                 // Cambio de sitio (o no)
                 ji = r_distribution(generator);
                 if (ji > p)     //No está mal, hay que tener en cuenta que al ser grid global realmente ya he cambiado los siios al calucular Eprime
                 {
                     int aux;
-                    aux = grid.get(i1,j1);
-                    grid.set(i1,j1,grid.get(i2,j2));
-                    grid.set(i2,j2,aux);
+                    aux = ring.get(i1);
+                    ring.set(i1,ring.get(i2));
+                    ring.set(i2,aux);
                 }
                 
             }
-            fprintf(flips_data, "\n");
-            print(flips_data);
-            
-            //Aqui no lo estoy haciendo con el valor absoluto, aunque luego lo pruebo.
-            energy=grid.energy();
-            sum_epp+=energy/pow(n,2);
-            sum_eppsrd+=pow(energy/n,2);
-
-            result = grid.magnet_regions();
-            m_up = result.up;
-            m_down = result.down;
-            sum_m_up += m_up;
-            sum_m_down += m_down;
-            sum_msqrd_up+=pow(m_up,2); sum_msqrd_down+=pow(m_down,2);
-            //fprintf(stdout, "eep: %lf \n", sum_epp);
-            
+            print(flips_data,t);
         }
-        
-        fprintf(magnetization_data,"%.1f, %f, %f \n",
-        temperature, sum_m_up/t_max, sum_m_down/t_max);
-        fprintf(epp,"%.1f, %lf \n",temperature,sum_epp/t_max );
-        fprintf(CV_dat,"%.1f, %lf \n",temperature,(sum_eppsrd-pow(n*sum_epp,2))/pow(t_max,2));
-        fprintf(Chi_dat, "%lf, %lf, %lf  \n", temperature,
-                (sum_msqrd_up - pow(abs(sum_m_up), 2)) / pow(t_max, 2), // Aquí me la estoy jugando poniendo el abs a la magnetización, formula del apartado 7
-                (sum_msqrd_down - pow(abs(sum_m_down), 2)) / pow(t_max, 2));
         fclose(flips_data);
     }
     fclose(magnetization_data);
-    fclose(epp);
-    fclose(CV_dat);
-    fclose(Chi_dat);
+    
 
     return 0;
 }
 
 
-double p_val(int i1, int i2, int j1, int j2, float temperature)
+double p_val(int i1, int i2, float temperature)
 {
     double p;
     double E,Eprime;
     int aux;
     //Energría de la configuración
-    E=grid.energy();
+    E=ring.energy();
     //Cambio por su vecino aleatorio y vuelvo a calcular la energía
-    aux = grid.get(i1, j1);
-    grid.set(i1, j1, grid.get(i2, j2));
-    grid.set(i2, j2, aux);
-    Eprime=grid.energy();
+    aux = ring.get(i1);
+    ring.set(i1, ring.get(i2));
+    ring.set(i2, aux);
+    Eprime=ring.energy();
     //Calculo la probabiliad de cambio de posición como
-    p=exp(-(Eprime-E)/temperature);
+    p=exp(-(Eprime-E)/temperature);//Función de partición Z con constante de Boltzman=1
     if (p >1){p=1;};
     return p;
 }
 
 
-void print(FILE *out)   //Solo para imprimir el grid
+void print(FILE *out,int t)   //Solo para imprimir el grid
 {
+    //fprintf(out,"%i ",t);
     for (int i = 0; i < n; i++)
     {
-        fprintf(out, "%i", grid.get(i,0));
-        for (int j = 1; j < n; j++)
-        {
-            fprintf(out, ",%i  ", grid.get(i,j));
-        }
-        fprintf(out, "\n");
+        int bin;
+        if (ring.get(i)==-1){bin=0;}else{bin=1;}
+        fprintf(out, "%i ",bin);
     }
+    fprintf(out, "\n");
+
     fflush(out);
 };
 
