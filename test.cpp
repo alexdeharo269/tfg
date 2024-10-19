74083c2b71c2f55b7bed04eba7ce2a62745db1b5
alexdeharo99 <alexdeharo269@gmail.com>
2024-10-20 00:03:59 +0200

    glauber

--- test.cpp
+++ test.cpp
@@ -11,18 +11,21 @@ using namespace std;
 //computo en cloud
 //calcular campo, modelasr dinamica glauber.
 
-unsigned int n = 100;
+unsigned int n = 50;
 unsigned int R = 5;
-const int t_max = 500; // Tiempo
+unsigned int Rg=0;
+const int t_max = 1000; // Tiempo
 const double alphadif = 1.0; // Probablidad de difusión
-const double alphareac = 0.0000;
+const double alphareac = 0.0000000001;
 float temperature_vals[] = {0.01f, 0.2f, 0.5f, 1.0f,9.0f}; // TEMPERATURAS ENTERAS DE 1 A 10
 
 vector<int> ring(n, 0);                                         
-vector<vector<int>> mat(n, vector<int>(n, 0));   //matriz de conectividad
+vector<vector<int>> matk(n, vector<int>(n, 0));   //matriz de conectividad
 vector<vector<int>> hk(n,vector<int>(n,0));      //campo dinamica kawasaki
- 
-unsigned seed1 = 19442448;   // semilla generador de números aleatorios
+vector<vector<int>> matg(n, vector<int>(n, 0));  // matriz de conectividad
+vector<int> hg(n, 0);    // campo dinamica glauber
+
+unsigned seed1 = 1944244;   // semilla generador de números aleatorios
 mt19937_64 generator(seed1); // generador  mt19937_64
 // Tenemos varias distribuciones de numeros aleatorios
 uniform_int_distribution<unsigned int> i_distribution(0, n - 1); // Distribución random para los i
@@ -30,6 +33,7 @@ uniform_real_distribution<double> r_distribution(0., 1.); // initialize the dist
 
 void initialize();
 void campokawa();
+void campoglaub();
 int count = 0;
 
 int main(){
@@ -42,17 +46,18 @@ int main(){
     {
         sum += ring[i];
     }
-    fprintf(stdout, " \n Magnetizacion inicial: %.4f", sum/n);
-    /*
-    for (unsigned int i = 0; i < n; i++)
+    fprintf(stdout, " \n Magnetizacion inicial: %.4f\n", sum/n);
+    
+    /*for (unsigned int i = 0; i < n; i++)
     {
         for (unsigned int j = 0; j < n; j++)
         {
-            fprintf(stdout, "%i", mat[i][j]);
+            fprintf(stdout, "%i", matg[i][j]);
+
         }
         fprintf(stdout,"\n");
     }*/
-
+    
     int t_size = sizeof(temperature_vals) / sizeof(temperature_vals[0]); // ¿Cuantas temperaturas hay?
     float temperature;
     char filename[35]; // Restringido a 35 caracteres, entonces solo caben en el nombre temperaturas menores de 10 y con 2 cifra decimal
@@ -77,6 +82,7 @@ int main(){
         for (t = 0; t < t_max; t++) // va en pasos Monte Carlo de n intentos de intercambio de posición o de spin flip
         {
             campokawa();
+            campoglaub();
 
             for (unsigned int try_ = 0; try_ < n; try_++)
             {
@@ -93,15 +99,18 @@ int main(){
                 //Para trabajar con índices unsigned teniendo en cuenta la condición circular.
                 unsigned low_bound=(i1>=R)? (i1-R):(i1+n-R);
                 unsigned up_bound=(i1+R)%n; 
+                unsigned int low_bound_reac=(i1>=R+Rg)?(i1-R-Rg):(i1-R-Rg+n);
+                unsigned int up_bound_reac=(i1+R+Rg)%n;
+                
                 //Este es el rango de difusión
                 if (((i2 >= low_bound) && (i2 <= up_bound)) ||
                     ((up_bound < low_bound) && ((i2 >= low_bound) || (i2 < up_bound))))
                 {
-
+                    
                     p = exp(-alphadif * (hk[i1][i2]) / temperature);
                     if (p >= 1)
                     {
-                        count++;
+                        //count++;
                         p = 1;
                     }
                     //  Cambio de sitio (o no),
@@ -111,22 +120,26 @@ int main(){
                         d = d + 1;
                         swap(ring[i1],ring[i2]);
                     }
+                    //continue;
                 }
-                //REACCIÓN
-                /*
-                else // if ((i2 > (i1 - R)) & (i2 < (i1 + R)))
+                else if (!(((i2 >= low_bound) && (i2 <= up_bound)) ||
+                           ((up_bound < low_bound) && ((i2 >= low_bound) || (i2 < up_bound)))))
                 {
-                    p = p_val_reac(i2, temperature);
-                    p = p * alphareac;
-
+                    if (((i2 >= low_bound_reac) && (i2 <= up_bound_reac)) ||
+                        ((up_bound_reac < low_bound_reac) && ((i2 >= low_bound_reac) || (i2 < up_bound_reac))))
+                    {
+                    // fprintf(stdout,"\n");
+                    double pg = exp(-alphareac*(hg[i1]) / temperature);
+                    if (pg>1){pg=1;}
                     // Cambio de spin
                     ji = r_distribution(generator);
-                    if (ji < p)
+                    if (ji < pg)
                     {
-
-                        ring[i2] = -ring[i2];
+                        count++;
+                        ring[i1] = -ring[i1];
                     }
-                }*/
+                    }
+                }
             }
 
             for (unsigned int i = 0; i < n; i++)
@@ -160,9 +173,14 @@ void initialize(){
             ring[i] = -1;
         }
 
-        for(unsigned int j=i+1;j<(i+R+1);j++){
-            mat[i][j % n] = 1;
-            mat[j%n][i]=1;
+        for(unsigned int j=i+1;j<=(i+R);j++){
+            matk[i][j % n] = 1;
+            matk[j%n][i]=1;
+        }
+
+        for(unsigned int j=i+1;j<=(i+R+Rg);j++){
+            matg[i][j%n]=1-matk[i][j%n];
+            matg[j % n][i] = 1 - matk[j%n][i];
         }
     }
 
@@ -179,11 +197,21 @@ void campokawa(){
         {
             int sum=0;
             for (unsigned int k=0;k<n;k++){
-                sum = sum + ring[k] * mat[i][k] - ring[k] * mat[j][k];
+                sum = sum + ring[k] * matk[i][k] - ring[k] * matk[j][k];
             }
-             sum=sum+mat[j][i]*ring[i]-mat[i][j]*ring[j];
+             sum=sum+matk[j][i]*ring[i]-matk[i][j]*ring[j];
             sum *= (ring[i] - ring[j]);
             hk[i][j]=sum;
         }
     }
+}
+
+void campoglaub(){
+    for(unsigned int i=0;i<n;i++){
+        int sum = 0;
+        for (unsigned int j=0;j<n;j++){
+            sum+=matg[i][j]*ring[j];
+        }
+        hg[i]=sum*2*ring[i];
+    }    
 }
