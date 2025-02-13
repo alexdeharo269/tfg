9db8e305889d0f76617b212f13ab21f2b0545f11
alexdeharo99 <alexdeharo269@gmail.com>
2025-02-13 13:56:00 +0100

    ajusto el tiempo, temperatura 0 pero corrijo los pasos MC

--- estudio_rango_R.cpp
+++ estudio_rango_R.cpp
@@ -16,11 +16,17 @@
 -Aparecerán más casos con la dinamica de glauber y reac dif, pero con esos ya le puedo presentar a joaquin y hacer figuras buenas.
 */
 
+//Con temperatura puede no llegar nunca al estado estacionario, aunque puedo correr la simulación como mucho hasta el tiempo que
+//la simulación a T=0 se congela o el doble, por ejemplo.
+
 /*
 Hacer un initialize_shuffle
+Si el analisis de quimeras lo sistematizo pasarlo entero a c++
 */
 
-
+/*
+Analizar para magnetización distinta de 0 y mandar correo. buscar en literatura cuantificación quimera.
+*/
 
 #include<stdio.h>
 #include<vector>
@@ -33,15 +39,15 @@ Hacer un initialize_shuffle
 #include<cstdlib>
 using namespace std;
 
-double pp = 1;
-
-unsigned int n =256;
+unsigned int n =1024;
 unsigned int Rg=0;
-const int t_max = 100000;// Tiempo
-unsigned int R_init=10;
-long long unsigned int r_size=10;
+const int t_max = 20000;// Tiempo
+unsigned int R_init=100;
+long long unsigned int r_size=6;
+long long unsigned int r_jump=50;
 float temperature_vals[] = {0.01f, 0.2f, 0.5f, 1.0f, 2.0f, 5.0f, 9.0f, 10.0f}; // TEMPERATURAS ENTERAS DE 1 A 10
-double temperature=2.00;
+double temperature=0.000000000001;
+const double avg_mag=0.75;
 
 vector<unsigned int>R_vals(r_size,0);
  
@@ -52,8 +58,7 @@ vector<int>init_ring(n,0);
 unsigned seed1 = 1944243;   // semilla generador de números aleatorios
 mt19937_64 generator(seed1); // generador  mt19937_64
 //random_device{}(); para generar numeros realmente aleatorios que paso como semillas
-//Como están los threads es lo más correcto porque cada simulación llega exactamente a la misma secuencia del 
-//generador de numeros pseudoaleatorios.
+
 
 
 void initialize();
@@ -68,31 +73,20 @@ int main()
 
     //initialize_dominio_central(64);
     initialize();
-    //Print a la matriz de conectividad
-
     double sum = 0;
     for (unsigned int i = 0; i < n; i++)
     {
         sum += init_ring[i];
     }
     fprintf(stdout, " \n Magnetizacion inicial: %.4f.\n Size: %i", sum/n,n);
-    /*
-    for (unsigned int i = 0; i < n; i++)
-    {
-        for (unsigned int j = 0; j < n; j++)
-        {
-            fprintf(stdout, "%i", matk[i][j]);
 
-        }
-        fprintf(stdout,"\n");
-    }*/
 
-    FILE *r_data = fopen("./ising_data/kawa_T0/r_data.dat", "w");
+    FILE *r_data = fopen("./ising_data/kawa_T0/Avg_mag=0.75/r_data.dat", "w");
     
     fprintf(stdout, "\nR_size= %llu", r_size);
     for (unsigned i = 0; i < r_size; i++)
     {
-        R_vals[i] = R_init+i + 1;
+        R_vals[i] = R_init+i*r_jump + 1;
     }
 
     char filename[256]; // Restringido a 42 caracteres, entonces solo caben en el nombre de R hasta 100.
@@ -116,7 +110,7 @@ int main()
         double length_tm1=0;
         int frozentime=0;
 
-        sprintf(filename, "./ising_data/kawa_T0/raw/ising_dataR%iT%1f.dat", R, temperature);
+        sprintf(filename, "./ising_data/kawa_T0/Avg_mag=0.75/raw/ising_dataR%iT%.1f.dat", R, temperature);
         
         FILE *flips_data = fopen(filename, "w");
         if (flips_data == NULL)
@@ -124,10 +118,6 @@ int main()
             perror("Error opening file: "); // Esto se asegura de que si hay algun error en la temperatura pase a la siguiente.
             r_iter++;
         }
-
-
-        //Thread-safe rnadonm number generator
-      //mt19937_64 local_generator(seed1 + static_cast<unsigned>(omp_get_thread_num())+ r_iter);
         mt19937_64 local_generator(seed1 + r_iter);
 
         uniform_int_distribution<unsigned int> i_distribution(0, n - 1); // Distribución random para los i
@@ -136,109 +126,52 @@ int main()
         vector<int> frozenring(n, 0);
         vector<int> ring = init_ring;
 
-        //Vuelvo al primer anillo que inicilcé:
-        ring=init_ring;
-
-        //fprintf(stdout, "\n%f", pp);
 
         unsigned int i1, i2;
         //double p;
         //double ji;
         int t = 0;
-        //int d = 0;
-        //int count=0;
         unsigned int low_bound;
         unsigned int up_bound;
-        //unsigned int low_bound_reac;
-        //unsigned int up_bound_reac;
         bool inR=false;
-        //bool inRg=false;
-        //int t_lim=t_max;
         int t_lim=1000;
         
+        ring=init_ring;
 
         for (t = 0; t < t_lim; t++) // va en pasos Monte Carlo de n intentos de intercambio de posición o de spin flip
         {
             frozenring = ring;
-            
-            /*
-            if(t==t_max/2){
-                //fprintf(stdout, "\n");
-                for (int i=0;i<n;i++){
-                    for(int j=0;j<n;j++){
-                        if(hk[i][j]!=hk[j][i]){
-                            //fprintf(stdout, "%i", hk[i][j]);
-                        }
-                        
-                    }
-                    //fprintf(stdout,"\n");
-                }
-            }*/
 
-            for (unsigned int try_ = 0; try_ < n / ((2 * R * pp / n + 2 * Rg * (1 - pp) / n)); try_++)
+            //for (unsigned int try_ = 0; try_ < n / ((2 * R * pp / n + 2 * Rg * (1 - pp) / n)); try_++)
+            for (unsigned int try_ = 0; try_ < n ; try_++)
+            
             {
                 i1 = i_distribution(generator);
                 i2 = i_distribution(generator);
-                /*
-                int s;
-                if(i1>i2){
-                    s=i1;
-                    i1=i2;
-                    i2=s;
-                }*/
+                
 
                 // Para trabajar con índices unsigned teniendo en cuenta la condición circular.
                 low_bound = (i1 >= R) ? (i1 - R) : (i1 + n - R);
                 up_bound = (i1 + R) % n;
-                //low_bound_reac = (i1 >= R + Rg) ? (i1 - R - Rg) : (i1 - R - Rg + n);
-                //up_bound_reac = (i1 + R + Rg) % n;
-                // Calculo a partir de i1 e i2 si está en rango R o Rg
+                
                 inR = (((i2 >= low_bound) && (i2 <= up_bound)) ||
                        ((up_bound < low_bound) && ((i2 >= low_bound) || (i2 < up_bound))));
                
 
                 double delta=campokawa(i1,i2,ring,matk);
+                /*
                 double p =std::min(exp(-delta / temperature),1.0);
                 
                 double ji = r_distribution(generator);
                 if ((ji<p)&&(inR)){
                     std::swap(frozenring[i1], frozenring[i2]);
-                }   
-
-                /*
-                int mov;
-                if(xr<pp){
-                    //dif
-                    //fprintf(stdout, "%f", xr);
-                    delta=alphadif*campokawa(i1,i2,ring,matk); mov=0;
                 }
-                else{//reac
-                    delta=alphareac*hg[i1]; mov=1;
-                }*/
-                
-                //En el limite de temperatura 0 sin reacción:
-                //if(delta<=0){swap(frozenring[i1], frozenring[i2]);}
-                /*
-                p = exp(-delta / temperature);
-                
-                if (delta <= 0)
+                */
+                if ((delta <= 0) && (inR))
                 {
-                    // count++;
-                    p = 1;
+                    std::swap(frozenring[i1], frozenring[i2]);
                 }
-                //  Cambio de sitio (o no),
-                ji = r_distribution(generator);
-                if (ji < p)
-                {
-                    if((mov==0)&&(inR)){
-                        d = d + 1;
-                        swap(frozenring[i1], frozenring[i2]);
-                    }
-                    if((mov==1)&&(inRg)&&(!inR)){
-                        count++;
-                        frozenring[i1] = -frozenring[i1];
-                    }
-                }*/
+                
             }
 
             ring = frozenring;
@@ -261,11 +194,7 @@ int main()
             
         }
         fclose(flips_data);
-        //fprintf(stdout,"\nT=%.1f  ex=%i  sf=%i  thread=%i",temperature,d,count,omp_get_thread_num());
         mean_domain_length = mdl(ring);
-
-        //fprintf(stdout, "\nR=%i  MDL=%.2f, thread=%i,  Tr=%i", R, mean_domain_length,
-        //        omp_get_thread_num(), frozentime);
         fprintf(stdout, "\nR=%i  MDL=%.2f, Frozen time=%i", R, mean_domain_length,
                 frozentime);
         #pragma omp critical
@@ -277,11 +206,7 @@ int main()
         for (unsigned int i = 0; i < n; i++)
         {
             sumf += ring[i];
-        }
-        if (sumf != sum)
-        {
-            fprintf(stdout, "Conservación magnetización violada R=%u, %f!=%f",r_iter, sum / n, sumf / n);
-        }
+        }if (sumf != sum){fprintf(stdout, "Conservación magnetización violada R=%u, %f!=%f",r_iter, sum / n, sumf / n);}
     }
     fclose(r_data);
     auto end = std::chrono::system_clock::now();
@@ -295,7 +220,7 @@ void initialize(){
     for (unsigned int i = 0; i < n; i++)
     {
         s = r_distribution(generator);
-        if (s < 0.5)
+        if (s < avg_mag)
         {
             init_ring[i] = 1;
         }
@@ -332,7 +257,6 @@ double mdl(vector<int> &ring){
     return n/k;
 }
 
-// Vamos a añadir la función de inicializacion con un dominio central gordo y el resto aleatorio
 void initialize_dominio_central(unsigned ancho)
 {
     double s;
